b9b16f3667f2646d1f5abc2800bfc39181c2555a
Jane <lulujinglebell@gmail.com>
2017-07-13 23:01:13 -0700

    Task for tomorrow - implement a game that will ask for 3 inputs (left index, right index, chance) , then it will generate a random number and ask user to guess.

---
+++ Jane_c_test/Jane_c_test/Games/NumberGuessingGame.c
@@ -0,0 +1,9 @@
+//
+//  NumberGuessingGame.c
+//  Jane_c_test
+//
+//  Created by Jane Jane on 7/13/17.
+//  Copyright © 2017 Johnny. All rights reserved.
+//
+
+#include "NumberGuessingGame.h"
---
+++ Jane_c_test/Jane_c_test/Games/NumberGuessingGame.h
@@ -0,0 +1,16 @@
+//
+//  NumberGuessingGame.h
+//  Jane_c_test
+//
+//  Created by Jane Jane on 7/13/17.
+//  Copyright © 2017 Johnny. All rights reserved.
+//
+
+#ifndef NumberGuessingGame_h
+#define NumberGuessingGame_h
+
+#include <stdio.h>
+
+void numberGuessingGame();
+
+#endif /* NumberGuessingGame_h */
