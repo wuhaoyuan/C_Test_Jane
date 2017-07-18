f307c8427dc44036e6559467dc63a00291159d5c
Jane <lulujinglebell@gmail.com>
2017-07-17 23:07:05 -0700

    Implemented a number guessing game. Remember to print some message to interact with player. The shorter the message the better. Used scanf() and rand().

--- Jane_c_test/Jane_c_test/Games/NumberGuessingGame.c
+++ Jane_c_test/Jane_c_test/Games/NumberGuessingGame.c
@@ -7,3 +7,52 @@
 //
 
 #include "NumberGuessingGame.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+
+void numberGuessingGame() {
+    int leftInt;
+    int rightInt;
+    int changes;
+    int randomNumber_old;
+    int randomNumber;
+    int guessNumber;
+    int times = 0;
+    
+    // ask for the left interval, right interval and number of changes
+    printf("Input left interger, right interger and changes. (left right chances)\n");
+    scanf("%d %d %d", &leftInt, &rightInt, &changes);
+    
+    // generate a random number (secret number) from the user key in interval
+    // rand(void) returns a pseudo-random number in the range of 0 to RAND_MAX-32767.
+    srand((unsigned int)time(NULL));
+    randomNumber_old = rand();
+    randomNumber = randomNumber_old % (rightInt + 1 - leftInt) + leftInt;
+    
+    while (times < changes) {
+        
+        printf("Key in a interger: \n");
+        // ask user to key in a interger
+        scanf("%d", &guessNumber);
+        
+        if (guessNumber > randomNumber) {
+            printf("Too large!\n");
+        } else if (guessNumber < randomNumber) {
+            printf("Too small!\n");
+        } else {
+            printf("Bingo!!\n");
+            return;
+        }
+        printf("Changes: %d\n\n", changes-times-1);
+        
+        times++;
+    }
+    
+    //guess time is greater than changes
+    printf("You failed, sucker!\n");
+    printf("Secret number: %d\n", randomNumber);
+    
+    return;
+}
